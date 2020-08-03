3d5ba3d232591f8d03caa913d33e3c576c6a5186
Asfandyar Khan <gulshierkhan@gmail.com>
2020-08-03 12:08:30 -0400

    Update and rename numberInReverse to numberInReverse.java

--- numberInReverse.java
+++ numberInReverse.java
@@ -1,14 +1,12 @@
 public class numbersInReverse {
 
-	public static void main(String[] args) {
-			  
+	public static void main(String[] args) {	  
 		int number = 28362139; 
 
 		while (number > 0) {
 			System.out.println( number % 10);
-			 
 			number = number / 10;
-	   }
+	   	}
 	}
 
 }
